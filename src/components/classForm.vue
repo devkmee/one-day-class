03d3966a7898a4439a22a254adfeab309474bff1
devKmee <82103@DESKTOP-OB7C880>
2023-07-20 00:12:54 +0900

    클래스 등록 UI

--- src/components/classForm.vue
+++ src/components/classForm.vue
@@ -1,34 +1,98 @@
 <template>
-  <section class="houses" id="houses">
-    <div class="container">
-      <div class="section-header">
-        <div class="section-header__content">
-          <h4 class="sub-title">CLASS</h4>
-          <h2 class="title">원데이 클래스 등록</h2>
+  <div>
+    <!-- contact-us section start -->
+    <section class="contact-us" id="contact-us">
+      <div class="container">
+        <div class="section-header">
+          <div class="section-header__content">
+            <h4 class="sub-title">CLASS</h4>
+            <h2 class="title">클래스 등록하기</h2>
+          </div>
+        </div>
+        <div class="row justify-content-between align-items-center">
+          <div class="col-md-5">
+            <div class="section-banner">
+              <img
+                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                class="card-img-top"
+                alt="..."
+              />
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="section-header__content v2">
+              <form>
+                <div class="mb-5">
+                  <label class="form-label">클래스 이름</label>
+                  <input type="password" name="clsName" class="form-control" />
+                </div>
+                <div class="flex-box">
+                  <div>
+                    <label class="form-label">카테고리</label>
+                    <select
+                      class="form-select"
+                      name="cateCd"
+                      aria-label="Default select example"
+                    >
+                      <option selected>Open this select menu</option>
+                    </select>
+                  </div>
+                  <div>
+                    <label class="form-label" name="area">모집상태</label>
+                    <select
+                      class="form-select"
+                      name="status"
+                      aria-label="Default select example"
+                    >
+                      <option selected>Open this select menu</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="flex-box">
+                  <div>
+                    <label class="form-label">지역</label>
+                    <select
+                      class="form-select"
+                      name="area"
+                      aria-label="Default select example"
+                    >
+                      <option selected>Open this select menu</option>
+                    </select>
+                  </div>
+                  <div>
+                    <label class="form-label">가격</label>
+                    <input type="text" name="price" class="form-control" />
+                  </div>
+                </div>
+                <div class="flex-box">
+                  <div>
+                    <label class="form-label">최대 인원</label>
+                    <input type="text" name="studentMax" class="form-control" />
+                  </div>
+                  <div>
+                    <label class="form-label">수강시간 </label>
+                    <input type="text" name="time" class="form-control" />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="p-5 mb-2 bg-primary text-white rounded">
-      <form>
-        <ul class="icon-list">
-          <li>
-            <label>클래스 번호</label>
-            <span>값</span>
-          </li>
-          <li>
-            <select class="form-select" aria-label="Default select example">
-              <option selected>클래스 종류</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </li>
-        </ul>
-      </form>
-    </div>
-  </section>
+    </section>
+    <!-- contact-us section end -->
+  </div>
 </template>
 
 <script></script>
 
-<style></style>
+<style scoped>
+.flex-box {
+  display: flex;
+}
+.flex-box > div {
+  flex: 1;
+  margin: 0px 20px 40px 0px;
+}
+</style>
